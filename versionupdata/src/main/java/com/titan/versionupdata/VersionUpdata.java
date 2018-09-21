5bff9f1109f7edafc93db50437323c09b5d299e8
ljk761626336 <761626336@qq.com>
2018-09-21 18:39:19 +0800

    ===

--- versionupdata/src/main/java/com/titan/versionupdata/VersionUpdata.java
+++ versionupdata/src/main/java/com/titan/versionupdata/VersionUpdata.java
@@ -2,7 +2,6 @@ package com.titan.versionupdata;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Application;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
